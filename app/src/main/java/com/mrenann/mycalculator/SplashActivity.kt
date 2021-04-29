f5d832f6abd0fc1c6199d93ad3ec46f348edb732
Marcos Renann <marcos.renann.br@gmail.com>
2021-04-29 02:00:41 -0300

    Lottie Anim in SplashActivity

--- app/src/main/java/com/mrenann/mycalculator/SplashActivity.kt
+++ app/src/main/java/com/mrenann/mycalculator/SplashActivity.kt
@@ -1,11 +1,29 @@
 package com.mrenann.mycalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.mrenann.mycalculator.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        splashToOtherActivity()
+    }
+
+    private fun splashToOtherActivity(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            Intent(this, MainActivity::class.java).apply {
+                startActivity(this)
+            }
+            finish()
+        }, 2500)
     }
 }
